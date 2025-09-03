7ad09e799b0ae7654a6f79ddfc391590c63dd061
Your Name <you@example.com>
2025-09-03 22:26:13 +0500

    apod

---
+++ app/apod/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+import api from '@/lib/axios/api'
+import Image from 'next/image'
+import {useEffect, useState} from 'react'
+
+type Props = {
+	date: string
+	explanation: string
+	hdurl: string
+	title: string
+	url: string
+	media_type: 'image' | 'video'
+}
+
+const Apod = () => {
+	const [data, setData] = useState<Props | null>()
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await api.get<Props>(
+					`/planetary/apod?api_key=${process.env.NEXT_PUBLIC_NASA_KEY}`
+				)
+				setData(res.data)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+		fetchData()
+	}, [])
+	if (!data) return <p>Загрузка...</p>
+	return (
+		<>
+			<div className='flex flex-col justify-center items-center h-screen p-[50px] bg-pink-300'>
+				<div className='text-6xl mb-10 font-bold'>{data?.title}</div>
+				<div>
+					<Image
+						src={data?.url}
+						width={500}
+						height={500}
+						alt='Picture'
+						className='rounded-xl shadow mb-10'
+					/>
+				</div>
+				<div className='mb-10 text-justify'>
+					Description: {data?.explanation}
+				</div>
+				<div className='font-bold'>Date: {data?.date}</div>
+			</div>
+		</>
+	)
+}
+
+export default Apod
---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type {NextConfig} from 'next'
+
+const nextConfig: NextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'apod.nasa.gov',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
+}
+
+export default nextConfig
