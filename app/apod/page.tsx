cc264b12afa47502f210085b39951b783dbf5c55
wh <u.artem@crocos.kz>
2025-09-04 12:33:51 +0500

    serverside

--- app/apod/page.tsx
+++ app/apod/page.tsx
@@ -1,7 +1,5 @@
-'use client'
-import api from '@/lib/axios/api'
+// 'use client'
 import Image from 'next/image'
-import {useEffect, useState} from 'react'
 
 type Props = {
 	date: string
@@ -12,34 +10,49 @@ type Props = {
 	media_type: 'image' | 'video'
 }
 
-const Apod = () => {
-	const [data, setData] = useState<Props | null>()
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await api.get<Props>(
-					`/planetary/apod?api_key=${process.env.NEXT_PUBLIC_NASA_KEY}`
-				)
-				setData(res.data)
-			} catch (err) {
-				console.error(err)
-			}
+const Apod = async () => {
+	// const [data, setData] = useState<Props | null>()
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const res = await api.get<Props>(
+	// 				`/planetary/apod?api_key=${process.env.NEXT_PUBLIC_NASA_KEY}`
+	// 			)
+	// 			setData(res.data)
+	// 		} catch (err) {
+	// 			console.error(err)
+	// 		}
+	// 	}
+	// 	fetchData()
+	// }, [])
+	const res = await fetch(
+		`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`,
+		{
+			cache: 'no-store',
 		}
-		fetchData()
-	}, [])
+	)
+	const data: Props = await res.json()
 	if (!data) return <p className='text-center font-bold'>Загрузка...</p>
 	return (
 		<>
 			<div className='flex flex-col justify-center items-center h-screen p-[50px] bg-pink-300'>
 				<div className='text-6xl mb-10 font-bold'>{data?.title}</div>
 				<div>
-					<Image
-						src={data?.url}
-						width={500}
-						height={500}
-						alt='Picture'
-						className='rounded-xl shadow mb-10'
-					/>
+					{data.media_type === 'image' ? (
+						<Image
+							src={data.url}
+							width={500}
+							height={500}
+							alt={data.title}
+							className='rounded-xl shadow mb-10'
+						/>
+					) : (
+						<iframe
+							src={data.url}
+							className='w-[500px] h-[300px] mb-10 rounded-xl shadow'
+							allowFullScreen
+						/>
+					)}
 				</div>
 				<div className='mb-10 text-justify'>
 					Description: {data?.explanation}
